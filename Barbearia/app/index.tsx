4a0cf18e022e4527ef2732dc1c152b7cfb3aced8
YURI FERNANDES DE OLIVEIRA <202208614744@alunos.estacio.br>
2025-09-12 23:31:50 -0300

    Categorias

--- Barbearia/app/index.tsx
+++ Barbearia/app/index.tsx
@@ -7,6 +7,8 @@ import React from "react";
 export default function Page() {
   return (
     <View style={styles.container}>
+
+      {/*Inicio e menu Hamburger */}
       <View style={styles.header}>
         <View style={styles.leftContainer}>
           <Ionicons name="cut" style={styles.iconbarber} />
@@ -17,29 +19,52 @@ export default function Page() {
           <MaterialIcons name="menu" size={30} color="white" />
         </Pressable>
       </View>
-      
-      
+
+      {/* Input e btn de pesquisas */}
       <View style={styles.pesquisaContainer}>
 
-      <Text style={styles.nomeH1}>Olá, Yuri</Text>
-      <Text style={styles.hora}>Sexta, 25 de Abril</Text>
+        <Text style={styles.nomeH1}>Olá, Yuri</Text>
+        <Text style={styles.hora}>Sexta, 25 de Abril</Text>
 
       </View>
 
       <View style={styles.containerInput}>
-        <TextInput  placeholder="Faça sua busca...."  placeholderTextColor="" style={styles.pesquisaInput}/>
+        <TextInput placeholder="Faça sua busca...." placeholderTextColor="" style={styles.pesquisaInput} />
         <Ionicons name="search" size={30} color="white" style={styles.btnpesquisa} />
       </View>
-        
-      
 
-      
-      
+
+      {/* Categorias */}
+
+      <View style={styles.categoriasContainer}>
+
+        <View style={styles.categorias}>
+          <Ionicons name="cut" size={30} color="#b31010ff" />
+          <Text style={styles.categoriasTexto1}>Cortes</Text>
+        </View>
+
+        <View style={styles.categorias}>
+          <Ionicons name="cut" size={30} color="#b31010ff" />
+          <Text style={styles.categoriasTexto1}>Barba</Text>
+        </View>
+
+        <View style={styles.categorias}>
+          <Ionicons name="cut" size={30} color="#b31010ff" />
+          <Text style={styles.categoriasTexto1}>Acabamento</Text>
+        </View>
+        
+
+      </View>
+
+
+
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  // container do projeto todo
   container: {
     flex: 1,
     backgroundColor: 'black',
@@ -49,28 +74,30 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     overflow: 'hidden',
   },
+
+   // inicio e  iconer de hamburger
   header: {
     width: '100%',
     height: 'auto',
     flexDirection: 'row',
-    justifyContent: 'space-between', // Distribui os itens ao longo da linha
-    alignItems: 'center', // Centraliza verticalmente os itens
+    justifyContent: 'space-between', 
+    alignItems: 'center', 
     backgroundColor: '#1d1d1d',
     padding: 20,
   },
   leftContainer: {
-    flexDirection: 'row', // Alinha o ícone e o texto na horizontal
-    alignItems: 'center', // Centraliza verticalmente o ícone e o texto
+    flexDirection: 'row', 
+    alignItems: 'center', 
   },
   iconbarber: {
     color: 'red',
     fontSize: 40,
-    marginRight: 4, // Espaço entre o ícone e o texto
+    marginRight: 4, 
   },
   cutText1: {
     color: 'white',
     fontSize: 30,
-    marginLeft: 4, // Espaço opcional entre o texto e o ícone
+    marginLeft: 4, 
   },
 
 
@@ -89,19 +116,21 @@ const styles = StyleSheet.create({
     fontSize: 12,
     fontWeight: 'bold',
     marginLeft: 10
-   
-  },
 
+  },
+  
+  // contianer do input de pesquisa
   pesquisaInput: {
     backgroundColor: '#1d1d1d',
     width: '90%',
-    borderColor: 'white',
+    borderColor: 'red',
     height: 40,
     borderWidth: 1,
     borderRadius: 8,
     marginTop: 10,
     color: 'white',
     paddingLeft: 10,
+    fontSize: 15
 
   },
 
@@ -111,6 +140,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     width: '95%',
     color: 'white',
+
   },
 
   btnpesquisa: {
@@ -120,5 +150,39 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     marginTop: 10,
     marginLeft: 7
-  }
+  },
+
+
+  // seção de categorias
+
+  categoriasContainer: {
+    flexDirection: 'row',
+    gap: 10,
+    marginTop: 15,
+    paddingLeft: 10,
+    paddingRight: 10
+    
+    
+
+  },
+
+  categorias: {
+    flexDirection: 'row',
+    backgroundColor: '#1d1d1d',
+    padding: 5,
+    borderRadius: 6,
+    marginTop: 10,
+    gap: 2,
+    
+  },
+
+  categoriasTexto1: {
+    color: 'white',
+    fontSize: 15,
+    fontWeight: 'bold',
+    letterSpacing: 1,
+    padding: 5
+      
+    
+  },
 });
